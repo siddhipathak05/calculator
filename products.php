c3511e2f8b408737da555d7436322e811d03387b
siddhipathak05 <110470895+siddhipathak05@users.noreply.github.com>
2022-11-24 20:11:58 +0530

    Add files via upload

---
+++ products.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+        <!--jQuery library--> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+        <!--Latest compiled and minified JavaScript--> 
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Product</title>
+        <link rel="stylesheet" href="assignmentstyle.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top"> 
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand">Lifestyle Store</a>
+                </div>
+                <div class="collapse navbar-collapse" id="mynavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="cart.html"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
+                        <li><a href="settings.html"><span class="glyphicon glyphicon-user"></span>Settings</a></li>
+                        <li><a href="logout.html"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <div class="container">
+            <div id="place" class="jumbotron">
+                <h1>
+                    Welcome to our Lifestyle Store!
+                </h1>
+                <p>
+                    We have the best cameras, watches and shirts for you. No need to hunt around. We have all in one place.
+                </p>
+            </div> 
+        </div>
+        
+        <div class="container">
+           
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="thumbnail"><img src="5.jpg" alt="Responsive image">
+                        <div class="caption">
+                            <h3>
+                                Cannon EOS
+                            </h3>
+                            <p>
+                                Price: Rs.36000.00
+                            </p>
+                            <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6" >
+                    <div class="thumbnail"><img src="2.jpg" alt="Responsive image">
+                        <div class="caption">
+                            <h3>
+                                Sony DSLR
+                            </h3>
+                            <p>
+                                Price: Rs.40000.00
+                            </p>
+                            <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail"><img src="3.jpg" alt="Responsive image">
+                        <div class="caption">
+                            <h3>
+                                 Sony DSLR
+                            </h3>
+                            <p>
+                                Price: Rs.50000.00
+                            </p>
+                            <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail"><img src="4.jpg" alt="Responsive image">
+                        <div class="caption">
+                            <h3>
+                                Olympus DSLR
+                            </h3>
+                            <p>
+                                Price: Rs.80000.00
+                            </p>
+                            <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail"><img src="9.jpg" alt="Responsive image">
+                    <div class="caption">
+                        <h3>
+                            Titan Model #301
+                        </h3>
+                        <p>
+                            Price: Rs.130000.00
+                        </p>
+                        <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail"><img src="10.jpg" alt="Responsive image">
+                    <div class="caption">
+                        <h3>
+                            Titan Model #201
+                        </h3>
+                        <p>
+                            Price: Rs.3000.00
+                        </p>
+                        <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail"><img src="11.jpg" alt="Responsive image">
+                        <div class="caption">
+                            <h3>
+                                HMT Milan
+                            </h3>
+                            <p>
+                                Price: Rs.8000.00
+                            </p>
+                            <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                        </div>
+                    </div>
+                </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail"><img src="12.jpg" alt="Responsive image">
+                    <div class="caption">
+                        <h3>
+                            Faber Luba #111
+                        </h3>
+                        <p>
+                            Price: Rs.18000.00
+                        </p>
+                        <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail"><img src="8.jpg" alt="Responsive image">
+                    <div class="caption">
+                        <h3>
+                            H&W
+                        </h3>
+                        <p>
+                            Price: Rs.800.00
+                        </p>
+                        <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail"><img src="6.jpg" alt="Responsive image">
+                    <div class="caption">
+                        <h3>
+                            Luis Phil
+                        </h3>
+                        <p>
+                            Price: Rs.1000.00
+                        </p>
+                        <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail"><img src="13.jpg" alt="Responsive image">
+                    <div class="caption">
+                        <h3>
+                            John Zok
+                        </h3>
+                        <p>
+                            Price: Rs.1500.00
+                        </p>
+                        <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail"><img src="14.jpg" alt="Responsive image">
+                    <div class="caption">
+                        <h3>
+                            Jhalsani
+                        </h3>
+                        <p>
+                            Price: Rs.1300.00
+                        </p>
+                        <input type="submit" value="Add to cart" class="btn btn-primary btn-block">
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+        <div id="footed" class="container">
+            <p>Copyright &copy Lifestyle Store. All Rights Reserved Contact Us: +91 90000 00000</p>
+        </div>
+    </body>
+</html>
